6bb50141e7f479d4ddf999d50eee08029c1c36d5
Mueller <Brigitte.Mueller@uhn.ca>
2026-02-23 19:24:07 -0500

    changed background hatTop -0.17 instead of -0.1 (down from .21)

--- lib/violin_tuner_screen.dart
+++ lib/violin_tuner_screen.dart
@@ -472,7 +472,7 @@ class _ViolinTunerScreenState extends State<ViolinTunerScreen>
     final double travelWidth = screenWidth - hatSize;
     final double hatX = normalized * travelWidth;
 
-    final double hatTop = constraints.maxHeight * -0.15 + verticalDrop;
+    final double hatTop = constraints.maxHeight * -0.17 + verticalDrop;
 
     return AnimatedPositioned(
       duration: const Duration(milliseconds: 150),
